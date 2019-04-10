0e2ff59f278fbe6025032fd392d03a90eeee418c
livevikulin <47454540+livevikulin@users.noreply.github.com>
2019-04-10 16:26:08 +0700

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: ('.arrows__left'),
+        nextArrow: ('.arrows__right'),
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            } 
+        ]
+    });
+});
+    
